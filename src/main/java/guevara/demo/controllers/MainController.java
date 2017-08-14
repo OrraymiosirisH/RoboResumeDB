a1f0452691b7acaa808ad07358a6386a53c9d73a
OrraymiosirisH <ohguevara.alvarez@gmail.com>
2017-08-14 11:43:20 -0400

    The last commit part, Monday before noon.

--- src/main/java/guevara/demo/controllers/MainController.java
+++ src/main/java/guevara/demo/controllers/MainController.java
@@ -16,6 +16,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 
@@ -24,6 +25,8 @@ import javax.validation.Valid;
 
 @Controller
 public class MainController {
+
+    private int i;
 
     @Autowired
     ResumeRepository resumeRepository;
@@ -55,20 +58,21 @@ public class MainController {
         EducationalRepository educationalRepository;
 
         @GetMapping("/addedu")
-        public String loadEdu (Model toSend,@ModelAttribute("resume") Resume resume){
+        public String loadEdu (Model toSend){
             toSend.addAttribute("educational", new Educational());
             return "addedu";
         }
-
+    //,@ModelAttribute("resume") Resume resume
         @PostMapping("/addedu")
-        public String processEdu (@Valid @ModelAttribute("educational") Educational educational, @ModelAttribute("resume") Resume resume, BindingResult lista){
+        public String processEdu (@Valid @ModelAttribute("educational") Educational educational, BindingResult lista){
             if (lista.hasErrors()) {
                 return "addedu";
-
+               // @ModelAttribute("resume") Resume resume,
             }
 
             System.out.println("*adding a new Entry");
             educationalRepository.save(educational);
+
             return "message2";
         }
 
@@ -76,13 +80,13 @@ public class MainController {
     WorkRepository workRepository;
 
     @GetMapping("/addwork")
-    public String loadExp (Model toSend,@ModelAttribute("resume") Resume resume){
+    public String loadExp (Model toSend){
         toSend.addAttribute("work", new Work());
         return "addwork";
     }
 
     @PostMapping("/addwork")
-    public String processExp (@Valid @ModelAttribute("work") Work work, @ModelAttribute("resume") Resume resume, BindingResult audi){
+    public String processExp (@Valid @ModelAttribute("work") Work work, BindingResult audi){
         if (audi.hasErrors()) {
             return "addwork";
 
@@ -93,27 +97,32 @@ public class MainController {
         return "message3";
     }
 
+
     @Autowired
     SkillRepository skillRepository;
 
+
     @GetMapping("/addskill")
-    public String loadSk (Model toSend,@ModelAttribute("resume") Resume resume){
+    public String loadSk (Model toSend){
         toSend.addAttribute("skill", new Skill());
         return "addskill";
     }
 
     @PostMapping("/addskill")
-    public String processSk (@Valid @ModelAttribute("skill") Skill skill, @ModelAttribute("resume") Resume resume, BindingResult honda){
-        if (honda.hasErrors()) {
-            return "addskill";
+    public String processSk (@Valid @ModelAttribute("skill") Skill skill, BindingResult honda) {
 
-        }
+            if (honda.hasErrors()) {
+                return "addskill";
 
-        System.out.println("*adding a new Entry");
-        skillRepository.save(skill);
-        return "message4";
+            }
+
+            System.out.println("*adding a new Entry");
+            System.out.println("*adding a new Entry" + i++);
+            skillRepository.save(skill);
+            return "message4";
+
+
     }
-
 
     @GetMapping("/report")
     public String repoFinal(Model sendModel, @ModelAttribute("resume") Resume resume){
@@ -132,6 +141,18 @@ public class MainController {
 
         return "report";
     }
+
+    @RequestMapping("/final")
+
+    public String sayhello(){
+
+
+
+        return "final";
+
+
+    }
+
 
 
 
